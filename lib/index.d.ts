ed7b7c1a484c42e30ad16f694193ec3de207f96d
b68h4 <basargirginer@hotmail.com>
2022-09-25 05:16:50 +0300

    Pre-build

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { Call } from './call';
+import Contract from './contract';
+import Provider, { initMulticallProvider } from './provider';
+import { BlockTag, IMulticallProvider } from "./providerTypes";
+export { BlockTag, Call, Contract, Provider, initMulticallProvider, IMulticallProvider };
+//# sourceMappingURL=index.d.ts.map
